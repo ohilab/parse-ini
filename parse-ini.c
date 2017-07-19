f911fb40778e37178d26b0b2013d9e8baeb275a0
Marco Giammarini <m.giammarini@warcomeb.it>
2017-07-19 12:28:48 +0200

    Added open and close functions. Added get function with parsing of uint8, uint16 and hour.

--- parse-ini.c
+++ parse-ini.c
@@ -3,6 +3,7 @@
  *
  * Authors:
  *  Matteo Civale
+ *  Marco Giammarini <m.giammarini@warcomeb.it>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -25,3 +26,186 @@
  ******************************************************************************/
 
 #include "parse-ini.h"
+
+#include "fat-mla/ff.h"
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define PARSEINI_STRING_BUFFER_LENGTH    100
+
+#define PARSEINI_STRING_SECTION_UNIX     "[%s]\n"
+#define PARSEINI_STRING_SECTION_WIN      "[%s]\r\n"
+
+static TCHAR ParseINI_buffer[PARSEINI_STRING_BUFFER_LENGTH];
+
+static ParseINI_Errors ParseINI_cast2u8 (const char* string, uint8_t length, void* param)
+{
+    if (length > 3) return PARSEINIERRORS_PARAM_WRONG_LENGTH;
+
+    if (dtu8(string,(uint8_t*)param,length) != ERRORS_UTILITY_CONVERSION_OK)
+        return PARSEINIERRORS_PARAM_WRONG_CONVERSION;
+    else
+        return PARSEINIERRORS_OK;
+}
+
+static ParseINI_Errors ParseINI_cast2u16 (const char* string, uint8_t length, void* param)
+{
+    if (length > 5) return PARSEINIERRORS_PARAM_WRONG_LENGTH;
+
+    if (dtu16(string,(uint16_t*)param,length) != ERRORS_UTILITY_CONVERSION_OK)
+        return PARSEINIERRORS_PARAM_WRONG_CONVERSION;
+    else
+        return PARSEINIERRORS_OK;
+}
+
+static ParseINI_Errors ParseINI_cast2hour (const char* string, uint8_t length, void* param)
+{
+    Time_TimeType* hours = (Time_TimeType*)param;
+    uint8_t hour, minute;
+    uint8_t hourLength, minuteLength;
+    char* ptr1;
+    char* ptr2;
+
+    // 2 char for hour, plus 1 char for separator and 2 char for minute
+    if (length > 6) return PARSEINIERRORS_PARAM_WRONG_LENGTH;
+
+    // Clear all the contents
+    hours->hours = 0;
+    hours->minutes = 0;
+    hours->seconds = 0;
+
+    ptr1 = string;
+    // Fix the second pointer to the separator
+    ptr2 = strchr(ptr1,':');
+    // Clear the separator with end string
+    *ptr2 = '\0';
+    ptr2++;
+
+    // Compute length for both string
+    hourLength = strlen(ptr1);
+    minuteLength = strlen(ptr2);
+    if (*(ptr2+minuteLength-2) == '\r')
+        minuteLength -= 2;
+    else
+        minuteLength -= 1;
+
+    // Convert the string into numbers
+    if (dtu8(ptr1,&hour,hourLength) != ERRORS_UTILITY_CONVERSION_OK)
+        return PARSEINIERRORS_PARAM_WRONG_CONVERSION;
+    if (dtu8(ptr2,&minute,minuteLength) != ERRORS_UTILITY_CONVERSION_OK)
+        return PARSEINIERRORS_PARAM_WRONG_CONVERSION;
+    // Check if the numbers are valid
+    if ((hour < 24) && (minute < 60))
+    {
+        hours->hours = hour;
+        hours->minutes = minute;
+        return PARSEINIERRORS_OK;
+    }
+    else
+    {
+        return PARSEINIERRORS_PARAM_WRONG_CONVERSION;
+    }
+}
+
+static ParseINI_Errors ParseINI_cast2ip (const char* string, uint8_t length, void* param)
+{
+    // FIXME
+}
+
+ParseINI_Errors ParseINI_open (ParseINI_FileHandle dev)
+{
+    // Open file in reading mode
+    if ((dev->fr = f_open(&(dev->file),dev->filePath,FA_READ)) != FR_OK)
+        return PARSEINIERRORS_OPEN_FAIL;
+
+    return PARSEINIERRORS_OK;
+}
+
+ParseINI_Errors ParseINI_close (ParseINI_FileHandle dev)
+{
+    // Close file
+    if ((dev->fr = f_close(&(dev->file))) != FR_OK)
+        return PARSEINIERRORS_CLOSE_FAIL;
+
+    return PARSEINIERRORS_OK;
+}
+
+ParseINI_Errors ParseINI_get (ParseINI_FileHandle dev,
+                              const char* section,
+                              const char* parameter,
+                              ParseINI_VarType type,
+                              void* value)
+{
+    bool isSectionFound = FALSE;
+    // Used to define section string with win format
+    static char sectionTagWin[20];
+    // Used to define section string with unix format
+    static char sectionTagUnix[20];
+    static char paramTag[20];
+    static uint8_t paramLength = 0;
+    // Pointer for reading file
+    uint8_t* head;
+    uint8_t* pointer;
+
+    if (&(dev->file) == 0)
+        return PARSEINIERRORS_FILE_NOT_OPEN;
+
+    // Move read pointer to beginning of the file
+    f_lseek(&(dev->file),0);
+
+    // Generation of dummy strings for section tag
+    sprintf(sectionTagWin,PARSEINI_STRING_SECTION_WIN, section);
+    sprintf(sectionTagUnix,PARSEINI_STRING_SECTION_UNIX, section);
+
+    while (head = f_gets(ParseINI_buffer,PARSEINI_STRING_BUFFER_LENGTH,&(dev->file)))
+    {
+        if (!strcmp(ParseINI_buffer,sectionTagWin) || !strcmp(ParseINI_buffer,sectionTagUnix))
+        {
+            isSectionFound = TRUE;
+            break;
+        }
+    }
+    // check if the section was found
+    if (!isSectionFound) return PARSEINIERRORS_SECTION_NOT_FOUND;
+
+    paramLength = sprintf(paramTag, "%s", parameter);
+    // Search for the param
+    while(head = f_gets(ParseINI_buffer, PARSEINI_STRING_BUFFER_LENGTH,&(dev->file)))
+    {
+        if (!strncmp(ParseINI_buffer,paramTag,paramLength))
+        {
+            // Point to the next char after the parameter name
+            pointer = ParseINI_buffer + paramLength;
+
+            // Trim blank and equal char
+            while ((*pointer == ' ') || (*pointer == '=')) pointer++;
+
+            // Compute the value length
+            paramLength = strlen(pointer);
+            if (*(pointer+paramLength-2) == '\r')
+                paramLength -= 2;
+            else
+                paramLength -= 1;
+
+            switch (type)
+            {
+            case PARSEINIVARTYPE_U8:
+                return ParseINI_cast2u8(pointer,paramLength,value);
+            case PARSEINIVARTYPE_U16:
+            case PARSEINIVARTYPE_I16: // FIXME
+                return ParseINI_cast2u16(pointer,paramLength,value);
+            case PARSEINIVARTYPE_HOUR:
+                return ParseINI_cast2hour(pointer,paramLength,value);
+            case PARSEINIVARTYPE_FLOAT:
+            case PARSEINIVARTYPE_STRING:
+            case PARSEINIVARTYPE_IP:
+            default:
+                return PARSEINIERRORS_WRONG_TYPE;
+            }
+        }
+    }
+    // The param was not found!
+    return PARSEINIERRORS_PARAM_NOT_FOUND;
+}
--- parse-ini.h
+++ parse-ini.h
@@ -3,6 +3,7 @@
  *
  * Authors:
  *  Matteo Civale
+ *  Marco Giammarini <m.giammarini@warcomeb.it>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -32,7 +33,89 @@
 #define PARSEINI_LIBRARY_VERSION_m   0
 #define PARSEINI_LIBRARY_TIME        0
 
+/*
+ * The user must define these label with ...
+ * The name of the define are:
+ *     #define PARSEINI_PATH_LENGTH_FILE     100
+ *     #define PARSEINI_PATH_LENGTH_DIR      120
+ */
+#ifndef __NO_BOARD_H
+#include "board.h"
+#endif
+
+#include "fat-mla/ff.h"
+
+typedef enum _ParseINI_Errors
+{
+    /**< No Errors */
+    PARSEINIERRORS_OK                    = 0x00,
+
+    PARSEINIERRORS_OPEN_FAIL,
+    PARSEINIERRORS_CLOSE_FAIL,
+    PARSEINIERRORS_FILE_NOT_OPEN,
+
+    PARSEINIERRORS_SECTION_NOT_FOUND,
+    PARSEINIERRORS_PARAM_NOT_FOUND,
+    PARSEINIERRORS_PARAM_WRONG_LENGTH,
+    PARSEINIERRORS_PARAM_WRONG_CONVERSION,
+    PARSEINIERRORS_WRONG_TYPE,
+
+} ParseINI_Errors;
+
+/**
+ *
+ */
+typedef enum _ParseINI_VarType
+{
+
+    PARSEINIVARTYPE_U8,
+    PARSEINIVARTYPE_U16,
+    PARSEINIVARTYPE_I16,
+    PARSEINIVARTYPE_FLOAT,
+    PARSEINIVARTYPE_STRING,
+    PARSEINIVARTYPE_IP,
+    PARSEINIVARTYPE_HOUR,
+
+} ParseINI_VarType;
 
 
+/**
+ *
+ */
+typedef struct _ParseINI_File
+{
+    FIL file;
+    FATFS* fat;
+    FRESULT fr;
+    uint8_t filePath[PARSEINI_PATH_LENGTH_FILE];
+    uint8_t dirPath[PARSEINI_PATH_LENGTH_DIR];
+} ParseINI_File;
+
+typedef ParseINI_File* ParseINI_FileHandle;
+
+/**
+ *
+ */
+ParseINI_Errors ParseINI_open (ParseINI_FileHandle dev);
+
+/**
+ *
+ */
+ParseINI_Errors ParseINI_close (ParseINI_FileHandle dev);
+
+/**
+ *
+ * @param[in] dev
+ * @param[in] section
+ * @param[in] parameter
+ * @param[in] type
+ * @param[out] value
+ * @return
+ */
+ParseINI_Errors ParseINI_get (ParseINI_FileHandle dev,
+                              const char* section,
+                              const char* parameter,
+                              ParseINI_VarType type,
+                              void* value);
 
 #endif /* __PARSE_INI_H */
